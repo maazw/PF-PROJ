6b3151b106bba49565623be9d814d8c8cc56be12
maazw <155550625+maazw@users.noreply.github.com>
2024-01-14 11:15:29 +0500

    Update game.cpp

--- game2.cpp
+++ game2.cpp
@@ -4,16 +4,16 @@
 #include <conio.h>
 #include <time.h>
 
-FILE *inp;  // data file reading and writing operators
+FILE *inp;
 FILE *outp;
 
-char c[30][21]; // variable for storing screen particles (pixels)
-int n[30][21];  // variable for checking
+char c[30][21];
+int n[30][21];
 int highscore;
-int contr, tuk = 0, score = 0, t = 0, bt = 0, birdx = 0, birdy = 0; // various variables for certain operations
-int err; // boolean for error detection
+int contr, tuk = 0, score = 0, t = 0, bt = 0, birdx = 0, birdy = 0;
+int err;
 
-void game();  // various functions
+void game();
 void screen();
 void pipes();
 void bird();
@@ -26,27 +26,28 @@ void credits();
 
 int main()
 {
-    srand(time(0));  // seeding random number gen, we will need it later;
-    inp = fopen("/Program Files/FlappyBird/options.txt", "r");  // opening file in which highscore is stored
-    if (inp != NULL) // if file opens successfully, it reads the highscore
+    srand((unsigned int)time(NULL));
+
+    inp = fopen("/Program Files/FlappyBird/options.txt", "r");
+    if (inp != NULL)
     {
         fscanf(inp, "%d", &highscore);
         fclose(inp);
-        err = 0;  // error will be false because the file opened successfully
+        err = 0;
     }
     else
     {
-        highscore = 0; // if the file doesn't exist, highscore will be 0, and err will be true
+        highscore = 0;
         err = 1;
     }
 
     int a = 0;
-    char sl; // selection variable
-    while (1) // loop for repeating actions after each start
+    char sl;
+    while (1)
     {
         if (a == 0)
             goto play;
-        if (a > 0)               // if you play not the first time, it will ask you if you want to play
+        if (a > 0)
         {
             score = 0;
             printf("Do you want to play again? [y/n] ");
@@ -57,51 +58,40 @@ int main()
                 goto play;
         }
     play:
-        menu(); // calling menu function
+        menu();
         scanf(" %c", &sl);
-        switch (sl) // menu selections
+        switch (sl)
         {
         case '1':
-        {
-            game(); // if you choose play, it calls function game
+            game();
             break;
-        }
-        case '2': // other selections - other functions
-        {
+        case '2':
             help();
             goto play;
             break;
-        }
         case '3':
-        {
             credits();
             goto play;
             break;
-        }
         case '4':
-        {
-            goto quit; // exits game
+            goto quit;
             break;
-        }
         default:
-        {
             goto play;
             break;
         }
-        }
-        a++; // variable for checking how many times you've played
+        a++;
     }
 quit:
-    printf("I quit."); // stops game, app closes.
-
+    printf("I quit.");
     return 0;
 }
 
-void game()  // function for playing the game
+void game()
 {
     int x, y;
     char s;
-    for (y = 0; y < 21; y++)  // setting screen
+    for (y = 0; y < 21; y++)
     {
         for (x = 0; x < 30; x++)
         {
@@ -117,57 +107,55 @@ void game()  // function for playing the game
             }
         }
     }
-    c[10][10] = '*';  // in these coordinates will be our bird, marked *
-    screen();      // calls func for showing the screen
-    while (1)       // loop starts, actual gameplay begins
+    c[10][10] = '*';
+    screen();
+    while (1)
     {
-        s = '~';  // default control variable
-        Sleep(0.2 * 1000);  // this sets how fast everything moves
-        t++;              // this is a variable for storing 'time', or how many times a loop passed
-        if (kbhit()) // if a key is pressed, certain operations are done for the bird to move up
+        s = '~';
+        Sleep(0.2 * 1000);
+        t++;
+        if (kbhit())
         {
-            s = getch();        // gets what key is pressed
+            s = getch();
             if (s != '~')
-                tuk = 1; // if it is not default, then 'tuk' will be equal to 1, meaning that the bird will fly up
+                tuk = 1;
         }
-        for (x = 0; x < 30; x++) // just setting ground
+        for (x = 0; x < 30; x++)
         {
             c[x][20] = '-';
             n[x][20] = 2;
         }
-        bird();                       // calls bird move function
-        checkscore();                 // checks score
-        if (gameover() == 1)
-            goto gameEnd;   // checks if the bird hits pipes, if yes, the game ends
-        pipes();                             // spawns and moves pipes
+        bird();
+        checkscore();
+        if (gameover())
+            goto gameEnd;
+        pipes();
         if (score > highscore)
-            highscore = score;  // i think this is clear
-        screen();                            // finally, calls the screen function to show everything.
-
+            highscore = score;
+        screen();
         if (tuk > 0)
-            tuk++;           // if a key is pressed, the bird will fly up 2 times.
+            tuk++;
         if (tuk == 3)
-            tuk = 0;          // after that, the bird falls
+            tuk = 0;
     }
-gameEnd:   // ends the game
+gameEnd:
     if (score > highscore)
         highscore = score;
-    if (err == 0)              // if hi-score file exists, it writes your new highscore there.
+    if (err == 0)
     {
         outp = fopen("/Program Files/FlappyBird/options.txt", "w");
         fprintf(outp, "%d", highscore);
         fclose(outp);
     }
-    screen();    // shows the ending screen, and returns to int main
+    screen();
     endgame();
-    return;
 }
 
-void screen()    // func for showing the screen
+void screen()
 {
     int x, y;
-    system("cls");    // clears the console
-    for (y = 0; y < 21; y++) // shows pixels on their coordinates, and your score
+    system("cls");
+    for (y = 0; y < 21; y++)
     {
         for (x = 0; x < 30; x++)
         {
@@ -181,34 +169,34 @@ void screen()    // func for showing the screen
     printf("Your Score: %d", score);
 }
 
-void pipes()  // pipe movement and spawn func
+void pipes()
 {
     int x, y, r;
-    if (t == 10)   // if time is 10, or the loop has passed 10 times, it spawns a new pipe
+    if (t == 10)
     {
-        r = (rand() % 11) + 5;  // generates a random number, which will be the pipe's hole center
-        for (y = 0; y < 20; y++)  // only the y coordinate is needed
+        r = (rand() % 11) + 5;
+        for (y = 0; y < 20; y++)
         {
-            c[29][y] = '|';  // sets pipe
-            n[29][y] = 2;    // n will be 2, for checking if the bird has hit it,
+            c[29][y] = '|';
+            n[29][y] = 2;
         }
-        c[29][r - 1] = ' ';  // sets hole
+        c[29][r - 1] = ' ';
         c[29][r] = ' ';
         c[29][r + 1] = ' ';
         n[29][r - 1] = 0;
         n[29][r] = 0;
         n[29][r + 1] = 0;
         t = 0;
-        goto mv; // moves pipes
+        goto mv;
     }
     else
         goto mv;
-mv:                 // pipe movement
-    for (y = 0; y < 20; y++)  // loops for generating coordinates
+mv:
+    for (y = 0; y < 20; y++)
     {
         for (x = 0; x < 30; x++)
         {
-            if (c[x][y] == '|')  // all the pipes will be moved left by 1
+            if (c[x][y] == '|')
             {
                 if (x > 0)
                 {
@@ -217,7 +205,7 @@ mv:                 // pipe movement
                     c[x][y] = ' ';
                     n[x][y] = 0;
                 }
-                if (x == 0)  // if the screen ends (x=0), the pipe will disappear, to prevent errors
+                if (x == 0)
                 {
                     c[x][y] = ' ';
                     n[x][y] = 0;
@@ -227,13 +215,13 @@ mv:                 // pipe movement
     }
 }
 
-void bird()   // bird movement function
+void bird()
 {
     int x, y;
-    if (tuk > 0) // if a key is pressed, the bird moves up
+    if (tuk > 0)
     {
         bt = 0;
-        for (y = 0; y < 20; y++)   // loops for finding bird coordinates
+        for (y = 0; y < 20; y++)
         {
             for (x = 0; x < 30; x++)
             {
@@ -241,17 +229,17 @@ void bird()   // bird movement function
                 {
                     if (y > 0)
                     {
-                        c[x][y - 1] = '*';  // bird moves up by 1
+                        c[x][y - 1] = '*';
                         c[x][y] = ' ';
-                        birdx = x;        // sets bird x coordinate
-                        birdy = y - 1;      // sets bird y coord
-                        return;         // returns to the game func
+                        birdx = x;
+                        birdy = y - 1;
+                        return;
                     }
                 }
             }
         }
     }
-    else   // if no key is pressed, the bird falls
+    else
     {
         bt++;
         for (y = 0; y < 20; y++)
@@ -260,9 +248,9 @@ void bird()   // bird movement function
             {
                 if (c[x][y] == '*')
                 {
-                    if (y < 20)  // if the bird is not on the ground
+                    if (y < 20)
                     {
-                        if (bt < 3)   // if bird time is lower than 3, it falls 1 pixel
+                        if (bt < 3)
                         {
                             c[x][y + 1] = '*';
                             c[x][y] = ' ';
@@ -270,7 +258,7 @@ void bird()   // bird movement function
                             birdy = y + 1;
                             return;
                         }
-                        else if (bt > 2 && bt < 5)  // more time has passed, faster the bird falls (acceleration)
+                        else if (bt > 2 && bt < 5)
                         {
                             c[x][y + 2] = '*';
                             c[x][y] = ' ';
@@ -289,7 +277,7 @@ void bird()   // bird movement function
                     }
                     else
                     {
-                        return;  // if the bird is already on the ground, the function returns to check for game over.
+                        return;
                     }
                 }
             }
@@ -297,12 +285,12 @@ void bird()   // bird movement function
     }
 }
 
-void checkscore()  // checks if the bird gained score
+void checkscore()
 {
     int y;
     for (y = 0; y < 20; y++)
     {
-        if (c[birdx][y] == '|')  // if the bird x coord is equal to the pipe's coord, you get 1 point
+        if (c[birdx][y] == '|')
         {
             score++;
             return;
@@ -310,18 +298,18 @@ void checkscore()  // checks if the bird gained score
     }
 }
 
-int gameover()  // checks if the bird has hit something
+int gameover()
 {
     int x, y, f = 0;
-    if (birdy > 19) // checks if the bird hits the ground
+    if (birdy > 19)
     {
-        c[birdx][19] = '*';  // sets bird and ground again, prevents errors
+        c[birdx][19] = '*';
         c[birdx][20] = '-';
-        f = 1;           // f=1, means the function will return true
+        f = 1;
         goto quit;
     }
     else
-    {     // checks if the bird has hit pipes, here the 'n' variable is needed (pipe's coordinate's n is equal 2 (more than 0))
+    {
         if (n[birdx][birdy] > 0 && (c[birdx][birdy] == '|' || c[birdx][birdy] == '*'))
         {
             c[birdx][birdy] = '|';
@@ -337,35 +325,36 @@ quit:
         return 0;
 }
 
-void endgame() // just some screens for certain actions
+void endgame()
 {
-    screen();   // this one pops up if the game ends
+    screen();
     printf("\n\n");
-    printf(" ------------------------------------------------------------------------- \n");
-    printf("|    *****      *     *       * ******       ****  *       ****** ****    |\n");
-    printf("|   *          * *    * *   * * *           *    *  *     * *     *   *   |\n");
-    printf("|   *  ****   *   *   *  * *  * *****       *    *   *   *  ****  ****    |\n");
-    printf("|   *  *  *  *******  *   *   * *           *    *    * *   *     * *     |\n");
-    printf("|    *****  *       * *       * ******       ****      *    ***** *   *   |\n");
-    printf(" ------------------------------------------------------------------------- \n");
+    printf("   _____   ___  ___  ___ _____   _____  _   _ ___________  \n");
+    printf("  |  __ \\ / _ \\ |  \\/  ||  ___| |  _  || | | |  ___| ___ \\ \n");
+    printf("  | |  \\// /_\\ \\| .  . || |__   | | | || | | | |__ | |_/ / \n");
+    printf("  | | __ |  _  || |\\/| ||  __|  | | | || | | |  __||    /  \n");
+    printf("  | |_\\ \\| | | || |  | || |___  \\ \\_/ /\\ \\_/ / |___| |\\ \\  \n");
+    printf("   \\____/\\_| |_/\\_|  |_/\\____/   \\___/  \\___/\\____/\\_| \\_| \n");
+    printf("                                                           \n");
     printf("\n\n");
     printf("                        Y O U R   S C O R E : %d\n\n", score);
     printf("                        H I G H   S C O R E : %d\n", highscore);
     printf("\n\n");
 }
 
-void menu() {
+void menu()
+{
     system("cls");
     printf("\n");
-    printf(" --------------------------------------------------------  \n");
-    printf("|                                                        | \n");
-    printf("|   **** *    **** **** **** *   *    ***  * ***  ***    | \n");
-    printf("|   *    *    *  * *  * *  * *   *    *  * * *  * *  *   | \n");
-    printf("|   ***  *    **** **** **** *****    ***  * ***  *  *   | \n");
-    printf("|   *    *    *  * *    *      *      *  * * *  * *  *   | \n");
-    printf("|   *    **** *  * *    *      *      ***  * *  * ***    | \n");
-    printf("|                                                        | \n");
-    printf(" --------------------------------------------------------  \n");
+    printf("  ______ _                          ______ _         _ \n");
+    printf("  |  ___| |                         | ___ (_)       | |\n");
+    printf("  | |_  | | __ _ _ __  _ __  _   _  | |_/ /_ _ __ __| |\n");
+    printf("  |  _| | |/ _` | '_ \\| '_ \\| | | | | ___ \\ | '__/ _` |\n");
+    printf("  | |   | | (_| | |_) | |_) | |_| | | |_/ / | | | (_| |\n");
+    printf("  \\_|   |_|\\__,_| .__/| .__/ \\__, | \\____/|_|_|  \\__,_|\n");
+    printf("                | |   | |     __/ |                    \n");
+    printf("                |_|   |_|    |___/                     \n");
+    printf("                                                       \n");
     printf("\n\n");
     printf("                  High Score:  %d\n\n", highscore);
     printf("\n\n");
@@ -376,24 +365,30 @@ void menu() {
     printf("                  4: Exit        \n\n");
 }
 
-void credits() {
+void credits()
+{
     char sel;
     system("cls");
-    while (1) {
+    while (1)
+    {
         printf("\n\n");
         printf("               Lead programmer: Shuja \n\n");
         printf("               Designer: Maaz \n\n");
         printf("Go back? [y/n]  ");
         scanf(" %c", &sel);
-        if (sel == 'y') return;
-        else system("cls");
+        if (sel == 'y')
+            return;
+        else
+            system("cls");
     }
 }
 
-void help() {
+void help()
+{
     char sel;
     system("cls");
-    while (1) {
+    while (1)
+    {
         printf("\n\n");
         printf("                   Controls: Press any key to fly up.         \n\n");
         printf("             Goal: Fly through the holes between the pipes.   \n");
@@ -403,7 +398,10 @@ void help() {
         printf("                          Are you ready? Go!                  \n\n\n\n");
         printf("Go back? [y/n]  ");
         scanf(" %c", &sel);
-        if (sel == 'y') return;
-        else system("cls");
+        if (sel == 'y')
+            return;
+        else
+            system("cls");
     }
 }
+
